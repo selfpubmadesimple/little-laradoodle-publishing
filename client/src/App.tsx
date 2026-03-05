5c573341a7b613e06adfdf02cb4575fb4923844b
selfpubmadesimple <april@selfpubmadesimple.com>
2026-03-05 03:21:02 -0500

    Add client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,36 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+
+import { Frame } from "@/pages/Frame";
+import { ChildrensBooks } from "@/pages/ChildrensBooks";
+import { NonFictionBooks } from "@/pages/NonFictionBooks";
+
+function Router() {
+  return (
+    <Switch>
+      {/* Add pages below */}
+      <Route path="/" component={Frame} />
+      <Route path="/childrens-books" component={ChildrensBooks} />
+      <Route path="/non-fiction-books" component={NonFictionBooks} />
+      {/* Fallback to 404 */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
